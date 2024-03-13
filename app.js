b6e97f7e723e100dd8eeaeb8bbbe36a53c11da2b
DilshodbekIshmominov <sayyodxonishmominova@gmail.com>
2024-03-13 15:56:51 +0500

    push

---
+++ app.js
@@ -0,0 +1,85 @@
+const btn = document.querySelector('.btn')
+const btn2 = document.querySelector('.btn2')
+const btn3 = document.querySelector('.btn3')
+const btn4 = document.querySelector('.btn4')
+const btn5 = document.querySelector('.btn5')
+
+
+
+
+
+btn.addEventListener('click', () =>{
+        btn.style.backgroundColor='blue'
+    btn.style.color='white'
+    btn2.style.backgroundColor='white'
+    btn2.style.color='black'
+    btn3.style.backgroundColor='white'
+    btn3.style.color='black'
+    btn4.style.backgroundColor='white'
+    btn4.style.color='black'
+    btn5.style.backgroundColor='white'
+    btn5.style.color='black'
+})
+btn2.addEventListener('click', () =>{
+    btn2.style.backgroundColor='blue'
+    btn2.style.color='white'
+    btn.style.backgroundColor='white'
+    btn.style.color='black'
+    btn3.style.backgroundColor='white'
+    btn3.style.color='black'
+    btn4.style.backgroundColor='white'
+    btn4.style.color='black'
+    btn5.style.backgroundColor='white'
+    btn5.style.color='black'
+})
+btn3.addEventListener('click', () =>{
+        btn3.style.backgroundColor='blue'
+        btn3.style.color='white'
+        btn.style.backgroundColor='white'
+        btn.style.color='black'
+        btn2.style.backgroundColor='white'
+        btn2.style.color='black'
+        btn4.style.backgroundColor='white'
+    btn4.style.color='black'
+    btn5.style.backgroundColor='white'
+    btn5.style.color='black'
+})
+btn4.addEventListener('click', () =>{
+        btn4.style.backgroundColor='blue'
+    btn4.style.color='white'
+    btn.style.backgroundColor='white'
+    btn.style.color='black'
+    btn2.style.backgroundColor='white'
+    btn2.style.color='black'
+    btn3.style.backgroundColor='white'
+    btn3.style.color='black'
+    btn5.style.backgroundColor='white'
+    btn5.style.color='black'
+})
+btn5.addEventListener('click', () =>{
+    btn5.style.backgroundColor='blue'
+    btn5.style.color='white'
+    btn.style.backgroundColor='white'
+    btn.style.color='black'
+    btn2.style.backgroundColor='white'
+    btn2.style.color='black'
+    btn3.style.backgroundColor='white'
+    btn3.style.color='black'
+    btn4.style.backgroundColor='white'
+    btn4.style.color='black'
+})
+
+
+
+
+
+
+// const btn = document.querySelectorAll('button')
+// btn.forEach(myFunction)
+
+//    function myFunction(item){
+//     btn.addEventListener('click',()=>{
+//         item.style.backgroundColor='blue'
+//         // if()
+//     })
+//    }
